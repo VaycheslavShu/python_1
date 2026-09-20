e6d9040dee721aefeea2882ab6eebd6b5a7114c0
Vyacheslav <wildhunter.85@yandex.ru>
2026-09-20 21:43:39 +0300

    win_1

--- python_homework1.py
+++ python_homework1.py
@@ -2,10 +2,20 @@ import os
 import sys
 
 
+def CountText(text:str)->str:
+    if len(text)%2==0:
+        mid = len(text) // 2
+        print(text[mid - 1:mid + 1])
+    else:
+        mid = len(text) // 2
+        print(text[mid])
 
 
-
+def main()->str:
+    word = input("Введите слово: ")
+    CountText(word)
 
 
 
 if __name__ == "__main__":
+    main()
